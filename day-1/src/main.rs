4b703e269afc3bd4e5bf5f1b35bceb30f5e6e0e4
SapientAsh <rowantreeofknowledge@gmail.com>
2022-12-01 11:02:48 -0800

    Completed day 1-1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::{BufReader, BufRead}};
+
+#[derive(Debug)]
+struct Elf {
+    calories: i32,
+}
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("SecondaryFiles/input.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut elves: Vec<Elf> = Vec::new();
+    let mut sum = 0;
+    for wrapped_line in reader.lines() {
+        let line = wrapped_line?;
+        if line == "" {
+            elves.push( Elf {
+                calories: sum
+            });
+            sum = 0;
+        }
+        else {
+            sum += line.parse::<i32>().unwrap();
+        }
+    }
+
+    let most_calories = elves.iter()
+    .reduce( |accum, item| {
+        if accum.calories >= item.calories { accum } else { item }
+    }).unwrap()
+    .calories;
+
+    println!("{most_calories} calories");
+
+    Ok(())
 }
