7f3b22a0eb668c076127e4b16d6d9f5e435aac8e
SapientAsh <rowantreeofknowledge@gmail.com>
2022-12-02 10:16:46 -0800

    Completed Day 2-2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -12,29 +12,29 @@ enum Move {
     Scissors,
 }
 
-fn evaluate_points(game: &Match) -> i32 {
-    let mut points = 0;
-    points += match game.player {
-        Move::Rock => 1,
-        Move::Paper => 2,
-        Move::Scissors => 3,
-    };
+fn part1() -> i32 {
+    fn evaluate_points(game: &Match) -> i32 {
+        let mut points = 0;
+        points += match game.player {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        };
 
-    if game.player == game.opponent {
-        points += 3;
+        if game.player == game.opponent {
+            points += 3;
+        }
+        else if 
+        game.player == Move::Rock     && game.opponent == Move::Scissors ||
+        game.player == Move::Scissors && game.opponent == Move::Paper    ||
+        game.player == Move::Paper    && game.opponent == Move::Rock     {
+            points += 6;
+        }
+
+        points
     }
-    else if 
-    game.player == Move::Rock     && game.opponent == Move::Scissors ||
-    game.player == Move::Scissors && game.opponent == Move::Paper    ||
-    game.player == Move::Paper    && game.opponent == Move::Rock     {
-        points += 6;
-    }
 
-    points
-}
-
-fn main() -> std::io::Result<()> {
-    let file = File::open("SecondaryFiles/input.txt")?;
+    let file = File::open("SecondaryFiles/input.txt").unwrap();
     let reader = BufReader::new(file);
 
     let tournament: Vec<Match> = reader.lines()
@@ -62,7 +62,38 @@ fn main() -> std::io::Result<()> {
         acc + evaluate_points(game)
     });
 
-    println!("Total points = {points}");
+    points
+}
 
-    Ok(())
+fn part2() -> i32 {
+    let file = File::open("SecondaryFiles/input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let points: i32 = reader.lines()
+    .map( | line | {
+        let line = line.unwrap();
+        let moves: Vec<&str> = line.split(" ").collect();
+        let opponent = {
+            if moves[0] == "A" { 0 }
+            else if moves[0] == "B" { 1 }
+            else { 2 }
+        };
+        
+        if moves[1] == "X" { 1 + ((opponent - 1) % 3 + 3) % 3 }
+        else if moves[1] == "Y" { 4 + opponent }
+        else { 7 + (opponent + 1) % 3 }
+    })
+    .collect::<Vec<i32>>()
+    .iter()
+    .sum();
+
+    points
+}
+
+fn main() {
+    let part1 = part1();
+    let part2 = part2();
+
+    println!("Part 1 = {part1}");
+    println!("Part 2 = {part2}");
 }
