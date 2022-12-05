943293afa35259a1f2a52b66a8f6448308d70895
SapientAsh <rowantreeofknowledge@gmail.com>
2022-12-04 21:48:21 -0800

    WIP Part 2 is nightmarish

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::{HashSet, HashMap}};
+use std::{fs::File, io::{BufReader, BufRead}, collections::HashSet};
 
 #[derive(Debug)]
 struct Bag {
@@ -6,7 +6,7 @@ struct Bag {
     compartment2: String,
 }
 
-fn points(item: char) -> u32 {
+fn priority(item: char) -> u32 {
     let ascii = item as u32;
     if ascii >= 65 && ascii <= 90 {
         return ascii - 38;
@@ -41,7 +41,7 @@ fn main() -> std::io::Result<()> {
                 acc + match letter1 == letter2 && !seen.contains(&letter1) {
                     true => {
                         seen.insert(letter1);
-                        points(letter1)
+                        priority(letter1)
                     }
                     false => 0,
                 }
@@ -50,33 +50,32 @@ fn main() -> std::io::Result<()> {
     })
     .sum();
 
+    let file = File::open("SecondaryFiles/fake_input.txt")?;
+    let reader = BufReader::new(file);
+
     let mut count = 0;
-    let mut seen = HashMap::new();
-    let part2: u32 = bags.iter()
-    .map( | item | {
+    let mut seen = vec![HashSet::new()];
+    let part2 = reader.lines()
+    .fold( 0, | acc, line | {
+        let line = line.unwrap();
         count += 1;
+        seen.push(HashSet::new());
+        let _: Vec<bool> = line.chars()
+        .map( | letter | seen[count].insert(letter))
+        .collect();
         if count % 3 == 0 {
-            seen = HashMap::new();
-        }
-        item.compartment1.chars()
-        .fold( 0, | acc, letter1 | {
-            acc + item.compartment2.chars()
-            .fold( 0, | acc, letter2 | {
-                acc + match letter1 == letter2 {
-                    true => {
-                        let times = seen.entry(letter1).or_insert(0);
-                        *times += 1;
-                        if count % 3 == 0 && seen.get(&letter1).copied().unwrap_or(0) == 3 {
-                            points(letter1)
-                        }
-                        else { 0 }
-                    }
-                    false => 0,
-                }
+            let mut temp = HashSet::new();
+            let _: Vec<bool> = seen[count - 2].intersection(&seen[count - 1])
+            .map( | item | temp.insert(*item))
+            .collect();
+            acc + temp.intersection(&seen[count])
+            .fold( 0, | acc, item | {
+                acc + priority(*item)
             })
-        })
-    })
-    .sum();
+        }
+        else { 0 }
+    });
+
 
     println!("Part 1: {part1}");
     println!("Part 2: {part2}");
